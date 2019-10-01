2ea9474a5ad88a366087be8c619664be85b5da83
Lian <liana.khachatryan1999@mail.ru>
2019-10-01 18:45:07 +0400

    Seconde Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Functions_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("your name: ");
+           name=Console.ReadLine();
+           for(int i=name.Length-1; i>=0;i--)
+           {
+               Console.Write(name[i]);
+           }
+
+           
+
+
+            
+
+
+
+        }
+    }
+}
